8170d26e45a4c49155d4f0ca67db78f5f2b831f7
Isabela Aquino <isabelaaquinom@outlook.com>
2022-02-25 16:48:45 -0400

    add ModoDark

--- script.js
+++ script.js
@@ -55,3 +55,14 @@ function resetCounter(){
     seconds = 00;
     updateCounterEl();
 }
+
+
+function darkMode() {
+    var element = document.body;
+    element.className = "dark-mode";
+  }
+
+  function lightMode() {
+    var element = document.body;
+    element.className = "light-mode";
+  }
